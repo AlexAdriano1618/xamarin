ee1cf1b782dd720e7f7c3e9508f62eb93faeba61
Alex Adriano <alexliga1998@outlook.com>
2018-04-28 18:35:06 -0500

    Vista login

---
+++ AppClases/AppClases/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppClases.ViewModels;
+namespace AppClases.Infrastructure
+{
+    
+    public class InstanceLocator
+    {
+        #region porpiedades
+        public MainViewModel Main
+        {
+            get;
+            set;
+
+        }
+        #endregion
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+
+        }
+        #endregion
+    }
+}
